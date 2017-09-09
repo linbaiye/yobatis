b1e3461794b5da2ef39d8248f307014a945e3169
Connor Lin <linbaiye@gmail.com>
2017-09-09 14:54:10 +0800

    Support parsing simple datasouce beans.

---
+++ src/main/java/org/nalby/yobatis/xml/BasicXmlParser.java
@@ -0,0 +1,31 @@
+package org.nalby.yobatis.xml;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
+import org.nalby.yobatis.util.Expect;
+
+public abstract class BasicXmlParser {
+	
+	private static final int MAX_STREAM_SIZE = 5 * 1024 * 1024;
+
+	Document document;
+	
+	public BasicXmlParser(InputStream inputStream, String rootElmentTag) throws DocumentException, IOException {
+		Expect.asTrue(inputStream != null
+				&& inputStream.available() <= MAX_STREAM_SIZE
+				&& inputStream.available() > 0,
+				"invalid input stream");
+		Expect.notNull(rootElmentTag, "root element tag is null.");
+		SAXReader saxReader = new SAXReader();
+		document = saxReader.read(inputStream);
+		if (document == null || document.getRootElement() == null
+				|| !rootElmentTag.equals(document.getRootElement().getName())) {
+			throw new DocumentException("Unpexpected document.");
+		}
+	}
+
+}
