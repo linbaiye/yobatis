0c448c54051937d0f2f70ce48eb3fe7b4b143d27
Connor Lin <linbaiye@gmail.com>
2017-09-16 16:21:31 +0800

    Add project configuration.

--- src/main/java/org/nalby/yobatis/util/Expect.java
+++ src/main/java/org/nalby/yobatis/util/Expect.java
@@ -2,14 +2,23 @@ package org.nalby.yobatis.util;
 
 public final class Expect {
 	private Expect() {}
+
 	public static void asTrue(boolean condition, String errMsg) {
 		if (!condition) {
 			throw new IllegalArgumentException(errMsg == null ? "condition is supposed to be true." : errMsg);
 		}
 	}
+
 	public static void notNull(Object object, String errMsg) {
 		if (object == null) {
 			throw new IllegalArgumentException(errMsg == null ? "null potintor passed." : errMsg);
+		}
+	}
+
+	public static void notEmpty(String object, String errMsg) {
+		notNull(object, errMsg);
+		if ("".equals(object.trim())) {
+			throw new IllegalArgumentException(errMsg == null ? "Empty string passed." : errMsg);
 		}
 	}
 }
